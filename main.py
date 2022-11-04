b20c1347da1475785c4b31bd4c05f0ebe1f50df0
Maksim Korolev <57666774+pkrfc@users.noreply.github.com>
2022-11-04 14:01:36 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,88 @@
+import requests as requests
+
+
+NM_ID = 116560924
+WIDTH = 12 #Ширина
+HEIGHT = 5 #Высота
+LONG = 19 #Длинна
+
+WB_TOKEN = (
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJ'
+    'RCI6ImNlMDJjMGFjLTM3NmQtNGVmYy1hNDE0LWI4OGM1MjY1N'
+    'TUyNyJ9.G5qZFNiwH6PVd6LwO0N3-x8uSeXQmf2KTVzYle8zgnU'
+)
+HEADERS = {
+    'Authorization': f'Bearer {WB_TOKEN}',
+    'Content-Type': 'application/json',
+}
+
+GET_CARD = 'https://suppliers-api.wildberries.ru/content/v1/cards/cursor/list'
+
+CET_CARD_FILTER = 'https://suppliers-api.wildberries.ru/content/v1/cards/filter'
+
+UPDATE_CARD = 'https://suppliers-api.wildberries.ru/content/v1/cards/update'
+
+
+def get_card():
+    data = {
+        "sort": {
+            "filter": {
+                "textSearch": str(NM_ID),
+                "withPhoto": -1
+            }
+        }
+    }
+    card_info_response = requests.post(GET_CARD, headers=HEADERS, json=data)
+    match card_info_response.status_code:
+        case 200:
+            card = card_info_response.json()
+        case _:
+            return 'Не нашёл карточку'
+    try:
+        vendor_code = card.get('data').get('cards')[0].get('vendorCode')
+    except IndexError:
+        return []
+    except AttributeError:
+        return []
+    return vendor_code
+
+
+def update_card(vendore_code):
+    data = {
+        "vendorCodes":
+            [vendore_code]
+    }
+    card_filter_response = requests.post(CET_CARD_FILTER, headers=HEADERS, json=data)
+    match card_filter_response.status_code:
+        case 200:
+            card_filter = card_filter_response.json()
+        case _:
+            return 'Не нашёл карточку'
+    card_list = card_filter.get('data')
+    for card in card_list:
+        if card.get("nmID") == NM_ID:
+            new_card = card
+    characteristics = new_card['characteristics']
+    new_characteristics = []
+    for item in characteristics:
+        if not item.get('Ширина упаковки') or item.get('Высота упаковки') or item.get('Длина упаковки'):
+            new_characteristics.append(item)
+
+    new_characteristics.append({'Ширина упаковки': WIDTH})
+    new_characteristics.append({'Высота упаковки': HEIGHT})
+    new_characteristics.append({'Длина упаковки': LONG})
+    new_card['characteristics'] = new_characteristics
+    new_card = [new_card, ]
+    card_update = requests.post(UPDATE_CARD, headers=HEADERS, json=new_card)
+    match card_update.status_code:
+        case 200:
+            return card_update.json()
+        case _:
+            return 'Не обновил'
+
+
+if __name__ == '__main__':
+    vendor_code = get_card()
+    print(update_card(vendor_code))
+
+
